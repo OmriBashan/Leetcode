2610b8fcb28693698c4fe943490df5956a1cbb6d
jtiz the developer <omri2511197@gmail.com>
2021-08-10 11:24:00 +0300

    Time: 80 ms (84.08%), Space: 41.1 MB (29.81%) - LeetHub

---
+++ symmetric-tree/symmetric-tree.js
@@ -0,0 +1,14 @@
+var isSymmetric = function(root) {
+    const isMirror = (t1, t2) => {
+        if (t1 === null && t2 === null) {
+            return true;
+        }
+        if (t1 === null || t2 === null) {
+            return false;
+        }
+        return t1.val === t2.val 
+            && isMirror(t1.left, t2.right) 
+            && isMirror(t1.right, t2.left);
+    };
+    return isMirror(root, root);
+};
